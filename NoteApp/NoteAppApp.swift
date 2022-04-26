57bf60146bf00bc91cdcbcfad6f247ff209b3030
takafumi11 <takafumi_noiri@dirigio.jp>
2022-04-26 15:43:30 +0900

    initial commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by 野入隆史 on 2022/04/26.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
